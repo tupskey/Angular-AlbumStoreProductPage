b3ab79a77214428ec2489a15747f28debae631a7
Olatunde Temitope <tupskey@gmail.com>
2019-10-25 15:20:39 +0100

    Completed Module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -9,7 +9,7 @@ export class ProductService {
   constructor(private _http: Http) { }
 
   getAlbum(id: number){
-    this._http.get(this._albumUrl).map((response) => 
+    return this._http.get(this._albumUrl).map((response) => 
     response.json());
   }
 
